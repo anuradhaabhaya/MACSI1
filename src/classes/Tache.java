5276ef2c75bb1418230229ef0de0a96ee658a19f
anuradhaabhaya <anuradha.abhaya@free.fr>
2013-03-23 21:10:46 +0100

    modifications classes

--- src/classes/Tache.java
+++ src/classes/Tache.java
@@ -1,5 +1,13 @@
 package classes;
 
+import java.util.Calendar;
+
 public class Tache {
+
+	public Tache(Integer charge, String objectifs, Calendar dateDebutAuPlusTot,
+			Calendar dateDebutAuPlusTard, Calendar dateFinAuPlusTot,
+			Calendar dateFinAuPlusTard, Calendar dateDebut, Calendar dateFin) {
+
+	}
 
 }
